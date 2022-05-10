5defa2e202fdea63ada7bcde0b4602cf008e3e46
Shloka <shlokareddy@gmail.com>
2022-05-09 20:40:10 -0700

    added icons to header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,25 @@
 import styles from '../styles/Header.module.css';
+import { BiHelpCircle } from 'react-icons/bi';
+import { BiMenu } from 'react-icons/bi';
+import { FaRegChartBar } from 'react-icons/fa';
+import { RiSettings3Line } from 'react-icons/ri';
 
 const Header = () => {
   return (
     <nav className={styles.header}>
+      <div className={styles.iconContainer}>
+        <BiMenu className={styles.icon}/>
+        <BiHelpCircle className={styles.icon}/>
+      </div>
       <h1 className={styles.title}>
         <span className={styles.titleOne}>word</span>
         <span className={styles.titleTwo}>-a-</span>
         <span className={styles.titleThree}>gram</span>
       </h1>
+      <div className={styles.iconContainer}>
+        <FaRegChartBar className={styles.icon}/>
+        <RiSettings3Line className={styles.icon}/>
+      </div>
     </nav>
   )
 }
