e18e61005770537183009451392edca075e1a1a1
Shloka <shlokareddy@gmail.com>
2022-05-17 19:50:18 -0700

    added to settings modal

--- components/Settings.js
+++ components/Settings.js
@@ -1,6 +1,9 @@
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 import styles from '../styles/Settings.module.css';
 
 const Settings = ({ open, setOpen }) => {
@@ -10,6 +13,17 @@ const Settings = ({ open, setOpen }) => {
     <Dialog open={open} onClose={() => setOpen(false)}>
       <DialogTitle>Settings</DialogTitle>
       <Divider />
+      <List>
+        <ListItem>
+          <ListItemText primary="Set Timer" />
+        </ListItem>
+        <ListItem>
+          <ListItemText primary="Zen Mode" />
+        </ListItem>
+      </List>
+      {/* <div>
+        <span>Timer</span>
+      </div> */}
     </Dialog>
 
   );
