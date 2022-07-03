db398f52efedf8fa32200b8c9d80eb5674fc9c10
Shloka <shlokareddy@gmail.com>
2022-07-03 12:02:56 -0700

    lobby modal edits

--- components/LobbyModal.js
+++ components/LobbyModal.js
@@ -4,10 +4,18 @@ import { motion } from "framer-motion";
 const LobbyModal = () => {
 
   return (
-    <motion.svg className={styles.modalCell} viewBox="0 0 120 103.92304845413263"
-        animate={{ scale: 4, x: "-38%", y: "-35%" }} transition={{ duration: 0.7 }}>
-        <polygon className={styles.hexagon} points="3,51.96152422706631 30,3 90,3 117,51.96152422706631 90,100.92304845413263 30,100.92304845413263" fill="url(#gradient)"></polygon>
-    </motion.svg>
+    <div className={styles.main}>
+      <motion.svg className={styles.modalCell} viewBox="0 0 120 103.92304845413263"
+          animate={{ scale: 4, x: "-40%", y:"-40%" }} transition={{ duration: 0.7 }}>
+          <polygon className={styles.hexagon} points="3,51.96152422706631 30,3 90,3 117,51.96152422706631 90,100.92304845413263 30,100.92304845413263" fill="url(#gradient)"></polygon>
+      </motion.svg>
+      <motion.div className={styles.buttons}
+        initial={{ opacity: 0}} animate={{ opacity: 1 }} transition={{ duration: 0.8 }}>
+        <button className={styles.button}>Create new game</button>
+        <span className={styles.or}>or</span>
+        <button className={styles.button}>Join by game link</button>
+      </motion.div>
+    </div>
   )
 }
 
