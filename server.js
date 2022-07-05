a477f73e61f2569e5654153c3139263851e17a08
Shloka <shlokareddy@gmail.com>
2022-07-04 17:35:40 -0700

    integrated custom express server

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const next = require('next')
+const io = require('socket.io')(8080)
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
+
+io.on('connection', socket => {
+  console.log(socket.id);
+})
+
+
+
+// const express = require('express')();
+// const app = express();
+// const http = require('http');
+// const server = http.createServer(app);
+
+// const { Server } = require("socket.io");
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*" //allowing cors from anywhere
+//   }
+// });
+
+// // app.get("/", (req, res) => {
+// //   res.send("Websocket Server")
+// // })
+
+// server.listen(3000, () => {
+//   console.log('listening on port 3000');
+// });
